440c3b0ba98be7c20cd9a8011214067dcf65f1df
Gabrielle Naubert <gnaubert@xelerance.com>
2019-07-15 21:40:41 -0400

    Adding references now works

--- assgn.js
+++ assgn.js
@@ -6,7 +6,7 @@ function addRef(){
 	a.appendChild(linkText);
 	a.title = "Link "+linknum;
 	var ref = prompt("Add a new reference link");
-	if(ref==null||){
+	if(ref==null){
 		return;
 	}
 	a.href = ref;
@@ -16,4 +16,19 @@ function addRef(){
 
 	var b = document.createElement('br');                        
 	document.getElementById("links").appendChild(b);          
+}
+function addTsk(){
+
+	var a = document.createElement('input');
+	var tsk = prompt("What's your next task?");
+	var linkText = document.createTextNode(tsk);
+	a.appendChild(linkText);
+	if(tsk==null){
+		return;
+	}  
+	a.type="checkbox";                        
+	document.getElementById("tsk").appendChild(a);
+
+	var b = document.createElement('br');                        
+	document.getElementById("tsk").appendChild(b);  
 }
