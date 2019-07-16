8e62aba3f850fd8f0d92ebd1374aae937a1c4a69
Gabrielle Naubert <gnaubert@xelerance.com>
2019-07-15 20:25:09 -0400

    implementing bootstrap for assignment page

--- assgn.js
+++ assgn.js
@@ -1,3 +1,19 @@
+var linknum = 5;
 function addRef(){
-	document.getElementById("links").innerHTML = "Paragraph changed.";
+
+	var a = document.createElement('a');
+	var linkText = document.createTextNode("Link "+linknum);
+	a.appendChild(linkText);
+	a.title = "Link "+linknum;
+	var ref = prompt("Add a new reference link");
+	if(ref==null||){
+		return;
+	}
+	a.href = ref;
+	a.target = "_blank";                            
+	document.getElementById("links").appendChild(a);
+	linknum++;
+
+	var b = document.createElement('br');                        
+	document.getElementById("links").appendChild(b);          
 }
